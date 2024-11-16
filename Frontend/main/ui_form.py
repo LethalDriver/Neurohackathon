7e505cd3d0e27a356b5462a0898022811a565418
Damian <damianpietron007@gmail.com>
2024-11-16 17:40:47 +0100

    Add dock widgets

--- Frontend/main/ui_form.py
+++ Frontend/main/ui_form.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDockWidget, QGridLayout, QListView,
-    QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QDockWidget, QGraphicsView, QGridLayout,
+    QHBoxLayout, QListView, QPushButton, QSizePolicy,
+    QTextEdit, QVBoxLayout, QWidget)
 
 class Ui_Widget(object):
     def setupUi(self, Widget):
@@ -31,14 +32,76 @@ class Ui_Widget(object):
         self.dockWidgetContents_3.setObjectName(u"dockWidgetContents_3")
         self.gridLayout_5 = QGridLayout(self.dockWidgetContents_3)
         self.gridLayout_5.setObjectName(u"gridLayout_5")
-        self.listView_2 = QListView(self.dockWidgetContents_3)
-        self.listView_2.setObjectName(u"listView_2")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.pushButton_3 = QPushButton(self.dockWidgetContents_3)
+        self.pushButton_3.setObjectName(u"pushButton_3")
 
-        self.gridLayout_5.addWidget(self.listView_2, 0, 0, 1, 1)
+        self.verticalLayout.addWidget(self.pushButton_3)
+
+        self.pushButton = QPushButton(self.dockWidgetContents_3)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.dockWidgetContents_3)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout.addWidget(self.pushButton_2)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.widget = QWidget(self.dockWidgetContents_3)
+        self.widget.setObjectName(u"widget")
+        self.gridLayout_6 = QGridLayout(self.widget)
+        self.gridLayout_6.setObjectName(u"gridLayout_6")
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.graphicsView = QGraphicsView(self.widget)
+        self.graphicsView.setObjectName(u"graphicsView")
+
+        self.verticalLayout_2.addWidget(self.graphicsView)
+
+        self.textEdit = QTextEdit(self.widget)
+        self.textEdit.setObjectName(u"textEdit")
+
+        self.verticalLayout_2.addWidget(self.textEdit)
+
+        self.pushButton_4 = QPushButton(self.widget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+
+        self.verticalLayout_2.addWidget(self.pushButton_4)
+
+
+        self.gridLayout_6.addLayout(self.verticalLayout_2, 0, 0, 1, 1)
+
+
+        self.horizontalLayout.addWidget(self.widget)
+
+
+        self.gridLayout_5.addLayout(self.horizontalLayout, 0, 0, 1, 1)
 
         self.dockWidget_2.setWidget(self.dockWidgetContents_3)
 
         self.gridLayout_4.addWidget(self.dockWidget_2, 0, 0, 1, 1)
+
+        self.dockWidget = QDockWidget(Widget)
+        self.dockWidget.setObjectName(u"dockWidget")
+        self.dockWidgetContents_2 = QWidget()
+        self.dockWidgetContents_2.setObjectName(u"dockWidgetContents_2")
+        self.gridLayout_7 = QGridLayout(self.dockWidgetContents_2)
+        self.gridLayout_7.setObjectName(u"gridLayout_7")
+        self.listView = QListView(self.dockWidgetContents_2)
+        self.listView.setObjectName(u"listView")
+
+        self.gridLayout_7.addWidget(self.listView, 0, 0, 1, 1)
+
+        self.dockWidget.setWidget(self.dockWidgetContents_2)
+
+        self.gridLayout_4.addWidget(self.dockWidget, 1, 0, 1, 1)
 
 
         self.retranslateUi(Widget)
@@ -48,5 +111,9 @@ class Ui_Widget(object):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Widget", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("Widget", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Widget", u"PushButton", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Widget", u"PushButton", None))
     # retranslateUi
 
